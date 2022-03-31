d3bb20497c7ca5f34bb18b2b02a081da4c652672
minzzang <minjjang1117@gmail.com>
2022-03-31 16:52:32 +0900

    feat: 멤버 auditing 테스트 수정

--- api/src/test/java/com/project/api/member/domain/MemberRepositoryTest.java
+++ api/src/test/java/com/project/api/member/domain/MemberRepositoryTest.java
@@ -1,14 +1,21 @@
 package com.project.api.member.domain;
 
+import com.project.api.config.JpaAuditingConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(
+        type = ASSIGNABLE_TYPE,
+        classes = {JpaAuditingConfig.class}
+))
 @Transactional
 class MemberRepositoryTest {
 
@@ -19,10 +26,10 @@ class MemberRepositoryTest {
     @DisplayName("멤버 auditing 테스트")
     public void findMember() {
         // given
-        Member member = new Member("1@gmail.com", "123");
+        Member member = Member.builder()
+                .email("minjjang@gmail.com").password("1234").build();
         // when
         Member saved = memberRepository.save(member);
-        System.out.println(saved);
         // then
         assertThat(saved.getCreatedAt()).isNotNull();
         assertThat(saved.getUpdatedAt()).isNotNull();
